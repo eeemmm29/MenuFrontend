a3f2ed16588f43d6a4896e9cd56ca2a499501ded
Emir <148708322+eeemmm29@users.noreply.github.com>
2025-04-23 17:42:41 +0600

    Set up running docker locally

---
+++ localRun.bat
@@ -0,0 +1,42 @@
+@echo off
+SET CONTAINER_NAME=menufrontend
+SET IMAGE_NAME=menufrontend
+REM Copy dev environment variables to .env
+copy .env.local .env
+REM Stop any running containers with the name 'menufrontend'
+docker ps -q -f name=%CONTAINER_NAME% >nul 2>nul
+IF NOT ERRORLEVEL 1 (
+    echo Stopping the running '%CONTAINER_NAME%' container...
+    docker stop %CONTAINER_NAME%
+    docker rm %CONTAINER_NAME%
+)
+
+REM Remove any previous images with the name 'menufrontend'
+docker images -q %IMAGE_NAME% >nul 2>nul
+IF NOT ERRORLEVEL 1 (
+    echo Removing the previous '%IMAGE_NAME%' Docker image...
+    docker rmi %IMAGE_NAME%
+)
+
+echo Building the Docker image...
+docker build -t %IMAGE_NAME% .
+IF ERRORLEVEL 1 (
+    echo Docker build failed. Exiting.
+    exit /b 1
+)
+
+echo Docker image built successfully.
+
+echo Running the Docker container...
+REM Note: You might need to pass environment variables like NEXT_PUBLIC_BACKEND_URL here
+REM Example: docker run --name %CONTAINER_NAME% -p 3000:3000 -e NEXT_PUBLIC_BACKEND_URL=http://localhost:8000 -d %IMAGE_NAME%
+docker run --name %CONTAINER_NAME% -p 3000:3000 --env-file .env -d %IMAGE_NAME%
+IF ERRORLEVEL 1 (
+    echo Docker container run failed. Exiting.
+    exit /b 1
+)
+
+echo Docker container is running.
+
+echo Opening http://localhost:3000 in the browser...
+start http://localhost:3000
